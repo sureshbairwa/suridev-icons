4f5debf8db375681b23f8ba63fa3eb8d08f05299
sureshbairwa <sureshbairwa@iitbhilai.ac.in>
2024-09-22 23:53:08 +0530

    Animation classes added

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
 
-import {
-  Circle,  Checkmark, Close, Close2, Close3, Delete,
-  Downarrow, Heart, Home, Leftarrow2, Lock, Menu, Minus, Plus, Profile, RightArrow2, Search, Trash, Unlock, Uparrow,
-  Ellipse, Hexagon, Parallelogram, Pentagon, Rectangle, Square, Star, Trapezoid, Triangle
-} from "./Icons/index";
-
-const iconsArray = [
-  Circle,Checkmark, Close, Close2, Close3, Delete,
-  Downarrow, Heart, Home, Leftarrow2, Lock, Menu, Minus, Plus, Profile, RightArrow2, Search, Trash, Unlock, Uparrow,
-  Ellipse, Hexagon, Parallelogram, Pentagon, Rectangle, Square, Star, Trapezoid, Triangle
-];
 
 
 
 function App() {
- 
   
 
   return (
     <div className="App">
-     
-
-      
-  
-     {/* {iconsArray.map((IconComponent, index) => (
-                <div key={index} className="icon-container">
-                    <IconComponent height="200" width="200" strokeWidth="1"   stroke="red"  opacity="1" className="three-color stroke-1to2  " />
-                    <p>{IconComponent.name}</p>  Displaying the name of the component 
-                </div>
-            ))} */}
-
-
-     
 
         
 
-    
-     
-    </div>
+           
+            
+             </div>
   );
 }
 
